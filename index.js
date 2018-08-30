44ba430d2590e8964afe9f3c98c11b811c9e4b09
smithwebtek <brad@smithwebtek.com>
2018-08-30 16:37:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-/**
- * Don't change these constants!
- */
 const DODGER = document.getElementById('dodger')
 const GAME = document.getElementById('game')
 const GAME_HEIGHT = 400
@@ -12,19 +9,8 @@ const START = document.getElementById('start')
 
 var gameInterval = null
 
-/**
- * Be aware of what's above this line,
- * but all of your work should happen below.
- */
-
 function checkCollision(rock) {
-	// implement me!
-	// use the comments below to guide you!
 	const top = positionToInteger(rock.style.top)
-
-	// rocks are 20px high
-	// DODGER is 20px high
-	// GAME_HEIGHT - 20 - 20 = 360px;
 	if (top > 360) {
 		const dodgerLeftEdge = positionToInteger(DODGER.style.left)
 		const dodgerRightEdge = dodgerLeftEdge + 40;
@@ -41,63 +27,25 @@ function checkCollision(rock) {
 
 function createRock(x) {
 	const rock = document.createElement('div')
-
 	rock.className = 'rock'
 	rock.style.left = `${x}px`
 
-	// Hmmm, why would we have used `var` here?
 	var top = rock.style.top = 0
-
-  /**
-   * Now that we have a rock, we'll need to append
-   * it to GAME and move it downwards.
-   */
 	GAME.appendChild(rock)
 
-  /**
-   * This function moves the rock. (2 pixels at a time
-   * seems like a good pace.)
-   */
 	function moveRock() {
-		// implement me!
-		// (use the comments below to guide you!)
-
 		rock.style.top = `${top += 2}px`;
-
-    /**
-     * If a rock collides with the DODGER,
-     * we should call endGame()
-     */
-
 		if (checkCollision(rock)) {
 			return endGame();
 		}
-
 		if (top < GAME_HEIGHT) {
 			window.requestAnimationFrame(moveRock)
 		} else {
 			rock.remove()
 		}
-
-
-
-
-    /**
-		 * Otherwise, if the rock hasn't reached the bottom of
-		 * the GAME, we want to move it again.
-     */
-
-		/**
-		 * But if the rock *has* reached the bottom of the GAME,
-		 * we should remove the rock from the DOM
-     */
 	}
 
-	// We should kick of the animation of the rock around here
 	window.requestAnimationFrame(moveRock)
-
-	// Add the rock to ROCKS so that we can remove all rocks
-	// when there's a collision
 	ROCKS.push(rock)
 
 	// Finally, return the rock element you've created
@@ -116,7 +64,9 @@ function endGame() {
 	ROCKS.forEach(function (rock) {
 		rock.remove()
 	})
+
 	window.removeEventListener('keydown', moveDodger)
+	// window.removeEventListener('keydown',)
 
 	START.innerHTML = "Play Again?"
 	START.style.display = 'inline'
@@ -126,13 +76,13 @@ function endGame() {
 
 function moveDodger(e) {
 	// implement me!
-  /**
-   * This function should call `moveDodgerLeft()`
-   * if the left arrow is pressed and `moveDodgerRight()`
-   * if the right arrow is pressed. (Check the constants
-   * we've declared for you above.)
-   * And be sure to use the functions declared below!
-   */
+	/**
+	 * This function should call `moveDodgerLeft()`
+	 * if the left arrow is pressed and `moveDodgerRight()`
+	 * if the right arrow is pressed. (Check the constants
+	 * we've declared for you above.)
+	 * And be sure to use the functions declared below!
+	 */
 
 	const code = e.which
 
@@ -150,10 +100,10 @@ function moveDodger(e) {
 
 function moveDodgerLeft() {
 	// implement me!
-  /**
-   * This function should move DODGER to the left
-   * (mabye 4 pixels?). Use window.requestAnimationFrame()!
-   */
+	/**
+	 * This function should move DODGER to the left
+	 * (mabye 4 pixels?). Use window.requestAnimationFrame()!
+	 */
 	window.requestAnimationFrame(function () {
 		const left = positionToInteger(DODGER.style.left)
 
@@ -166,10 +116,10 @@ function moveDodgerLeft() {
 
 function moveDodgerRight() {
 	// implement me!
-  /**
-   * This function should move DODGER to the right
-   * (mabye 4 pixels?). Use window.requestAnimationFrame()!
-   */
+	/**
+	 * This function should move DODGER to the right
+	 * (mabye 4 pixels?). Use window.requestAnimationFrame()!
+	 */
 	window.requestAnimationFrame(function () {
 		const left = positionToInteger(DODGER.style.left)
 
